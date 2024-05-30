20645ac963b1a4c16ed4532e66c99c7512675b9e
Emre Koçbaş <160711091+EmreKocbas@users.noreply.github.com>
2024-05-30 11:30:33 +0300

    weatherProject

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-
-
 import './App.css';
 import Province from './components/Province';
 import { WeatherProvider } from './context/weatherApi';
 
+// Define the main App component
 function App() {
   return (
-  <div>
-    <WeatherProvider>
-      <Province />
-  </WeatherProvider>
-    
-  </div>
-     
-    
+    <div>
+      {/* Wrap the Province component with WeatherProvider to provide context data */}
+      <WeatherProvider>
+        <Province />
+      </WeatherProvider>
+    </div>
   );
 }
 
